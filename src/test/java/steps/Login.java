9e672097bdc391849fb0fec05de9f1f3bdfa897a
Everton <evertonulisystem@gmail.com>
2021-09-25 22:38:29 -0300

    Projeto final com evidências do Teste

--- src/test/java/steps/Login.java
+++ src/test/java/steps/Login.java
@@ -1,22 +1,25 @@
 package steps;
 
+import com.aventstack.extentreports.utils.FileUtil;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
 
 public class Login {
-
-    private WebDriver driver;
+    public WebDriver driver;
 
     @Given("que acesso o conexaoQA")
     public void queAcessoOConexaoQA() {
-        System.setProperty("webdriver.chrome.driver","drivers/Chrome-chromedriver/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "drivers/Chrome-chromedriver/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MILLISECONDS);
@@ -25,14 +28,11 @@ public class Login {
 
     @And("clico no login")
     public void clicoNoLogin() {
-
-        //driver.findElement(By.className("a.btn.btn-light")).click();
-driver.findElement(By.cssSelector("section.landing div.dark-overlay div.landing-inner div.buttons > a.btn.btn-light:nth-child(2)")).click();
-
+        driver.findElement(By.cssSelector("section.landing div.dark-overlay div.landing-inner div.buttons > a.btn.btn-light:nth-child(2)")).click();
     }
 
     @When("coloco {string} e {string}")
-    public void colocoE(String usuario, String senha) {
+    public void colocoE(String usuario, String senha) throws IOException {
         driver.findElement(By.name("email")).click();
         driver.findElement(By.name("email")).sendKeys("evertonuli@bol.com.br");
         driver.findElement(By.name("password")).click();
@@ -42,6 +42,25 @@ driver.findElement(By.cssSelector("section.landing div.dark-overlay div.landing-
 
     @Then("sera exibido tela de dashboard")
     public void seraExibidoTelaDeDashboard() {
-        //driver.quit();
     }
+
+    @And("o botao de criar perfil estara sendo exibido")
+    public void oBotaoDeCriarPerfilEstaraSendoExibido() {
+    driver.findElement(By.cssSelector("body:nth-child(2) div:nth-child(2) section.container > a.btn.btn-primary.my-1:nth-child(4)")).click();
+    }
+
+    @And("visualizo a tela de criacao do perfil")
+    public void visualizoATelaDeCriacaoDoPerfil() throws IOException {
+
+        imprimirEvidencia();
+        //driver.quit();
+        }
+
+    public void imprimirEvidencia() throws IOException{
+        File printFinal = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(printFinal, new File("target/evidencia.png"));
+    }
+
 }
+
+
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,7 +1,10 @@
+
 Feature: Realizar login com cadastro ja criado
-  Scenario: Realizar Login no site
+  Scenario: Realize Login no site
     Given que acesso o conexaoQA
     And clico no login
     When coloco "usuario" e "senha"
     Then sera exibido tela de dashboard
+    And o botao de criar perfil estara sendo exibido
+    And visualizo a tela de criacao do perfil
 
